d9bb39281bd6842dd21c3406a38230c584aded0e
patree <treeshnapati@gmail.com>
2022-09-25 18:56:44 -0500

    updates inde.tsx and notes

---
+++ notes/notes.md
@@ -0,0 +1,9 @@
+Component (file): This term refers to a specific file in a software or computer
+system that is required for a specific function.
+
+Component (software): A component is a software object, intended to interact
+with other components, encapsulating certain functionality or a set of
+functionalities.
+
+Component (web): A component is a piece of UI. It can be a single element, such
+as a button, or a group of elements, such as a form.
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,61 @@
-import Counter from "../islands/Counter.tsx";
+import { Handlers } from "$fresh/server.ts";
 
-export default function Home() {
+//  Import Box components from island
+import Box from "../islands/Box.tsx";
+
+//  Import Item components from island
+import Item from "../islands/Item.tsx";
+
+// to load Environment variable
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+// get Environment variable
+let domain = Deno.env.get("DOMAIN");
+
+interface todo {
+  id: string;
+  title: string;
+  isDone: boolean;
+}
+
+//  Call get API with fresh handler
+export const handler: Handlers<todo | null> = {
+  async GET(_, ctx) {
+    // call get api
+    const response = await fetch(domain + "/api/get").then(
+      (response) => response.json(),
+    ).then(
+      (response) => JSON.parse(response),
+    ).catch(
+      (error) => console.log(error),
+    );
+
+    //  pass data into component props
+    return ctx.render(response);
+  },
+};
+
+export default function Home({ data }: { data: any }) {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class="h-screen w-screen flex flex-col items-center justify-center bg-blue-600 font-sans">
+      <div class="flex flex-row w-4/6 justify-center mx-auto">
+        <h2 class="m-2 p-1 text-5xl font-mono font-serif cursor-pointer">
+          Deno
+        </h2>
+        <h2 class="m-2 p-1 text-5xl font-mono font-serif cursor-pointer">
+          SurrealDB
+        </h2>
+      </div>
+
+      <div class="bg-white rounded shadow container mx-auto p-3 m-4 w-3/6 lg:w-3/6 xl:w-3/6 md:w-3/6 2xl:w-3/6 ">
+        <div class=" flex mb-4 flex-col py-2">
+          <h1 class="text-gray-500  text-lg">Todo List</h1>
+          <Box />
+        </div>
+        <div class="p-2">
+          {data.map((item: any) => <Item item={item} />)}
+        </div>
+      </div>
     </div>
   );
 }
